26fbdd8efca4e1f0b8e912591a28f4d0bcae6695
antoine <antoine.letellier90@gmail.com>
2025-03-14 11:40:47 +0100

    auto

--- setup.sh
+++ setup.sh
@@ -1,12 +1,8 @@
- #!/usr/bin/bash
-ln -s ~/dotfiles/nvim	~/.config/
-ln -s ~/dotfiles/kitty	~/.config/
-ln -s ~/dotfiles/tmux	~/.config/
-ln -s ~/dotfiles/.zshrc	~/
+#!/usr/bin/bash
+ln -s ~/dotfiles/nvim ~/.config/
+ln -s ~/dotfiles/.zshrc ~/
 
 python3 -m pip install --upgrade pip setuptools
 
-pipx install c-formatter-42
-pipx install norminette
 pipx install nodejs
 pipx install npm
