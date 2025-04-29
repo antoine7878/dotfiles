ce5d29e30a25483260cd117412c76541a72e8e55
Antoine Le Tellier <antoine.letellier90@gmail.com>
2025-04-29 09:59:39 +0200

    revamp

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
+
+######################
+
 # rm -rf ~/.config/nvim
 # rm -rf ~/.local/share/nvim
 # rm -rf ~/.local/state/nvim
@@ -7,16 +10,25 @@
 rm -rf ~/.config/nvim
 rm -rf ~/.config/zsh
 
-rm -rf ~/.ruff.toml
-rm -rf ~/.zshrc
 rm -rf ~/.clang-format
+rm -rf ~/.fonts
+rm -rf ~/.p10k.zsh
+rm -rf ~/powerlevel10k
+rm -rf ~/.prettierrc.yaml
+rm -rf ~/.ruff.toml
+rm -rf ~/.vimrc
 rm -rf ~/.zshenv
 
+######################
 
 ln -s ~/dotfiles/nvim ~/.config/
 ln -s ~/dotfiles/zsh ~/.config/
 
-ln -s ~/dotfiles/.zshrc ~/
 ln -s ~/dotfiles/.clang-format ~/
+ln -s ~/dotfiles/.fonts ~/
+ln -s ~/dotfiles/.p10k.zsh ~/
+ln -s ~/dotfiles/powerlevel10k ~/
+ln -s ~/dotfiles/.prettierrc.yaml ~/
 ln -s ~/dotfiles/.ruff.toml ~/
+ln -s ~/dotfiles/.vimrc ~/
 ln -s ~/dotfiles/.zshenv ~/
