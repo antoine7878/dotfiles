d1ab42e81c5f34ecd58b31a6d1e5faae404bcdd6
antoine7878 <antoine.letellier90@gmail.com>
2025-05-13 09:42:19 +0200

    b

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 ######################
 
-# rm -rf ~/.config/nvim
-# rm -rf ~/.local/share/nvim
-# rm -rf ~/.local/state/nvim
-# rm -rf ~/.cache/nvim
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+rm -rf ~/.local/state/nvim
+rm -rf ~/.cache/nvim
 
 rm -rf ~/.config/nvim
 rm -rf ~/.config/zsh
@@ -13,7 +13,7 @@ rm -rf ~/.config/zsh
 rm -rf ~/.clang-format
 rm -rf ~/.fonts
 rm -rf ~/.p10k.zsh
-rm -rf ~/powerlevel10k
+# rm -rf ~/powerlevel10k
 rm -rf ~/.prettierrc.yaml
 rm -rf ~/.ruff.toml
 rm -rf ~/.vimrc
@@ -27,7 +27,7 @@ ln -s ~/dotfiles/zsh ~/.config/
 ln -s ~/dotfiles/.clang-format ~/
 ln -s ~/dotfiles/.fonts ~/
 ln -s ~/dotfiles/.p10k.zsh ~/
-ln -s ~/dotfiles/powerlevel10k ~/
+# ln -s ~/dotfiles/powerlevel10k ~/
 ln -s ~/dotfiles/.prettierrc.yaml ~/
 ln -s ~/dotfiles/.ruff.toml ~/
 ln -s ~/dotfiles/.vimrc ~/
