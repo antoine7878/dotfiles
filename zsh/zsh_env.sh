f1be4d79d91884748e49097907236738fb28595a
antoine7878 <antoine.letellier90@gmail.com>
2025-07-09 15:29:25 +0200

    a

--- zsh/zsh_env.sh
+++ zsh/zsh_env.sh
@@ -15,9 +15,7 @@ export HOMEBREW_NO_ENV_HINTS
 export RUST_BACKTRACE=1
 
 
-if [[ "$(hostname)" == *paris42* ]]; then
 	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 	[[ ! -r '/home/ale-tell/.opam/opam-init/init.zsh' ]] || source '/home/ale-tell/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
 	export XDG_CONFIG_HOME="$HOME/.config"
 	source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-fi
